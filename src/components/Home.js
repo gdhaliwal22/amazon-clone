6924f02a3f93b8fd5caf917bc5c23f8dc89a20d3
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-25 14:29:38 -0700

    added dummy data of amazon products

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,13 +10,55 @@ function Home() {
         src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/Other/RB-2759_SVOD_DiamondMultititleJuly/Amazon_GW_DesktopTallHero_RB-2759_SVOD_DiamondMultititleJuly_MovieNight_1500x600._CB407968705_.jpg"
       />
       {/* Product id, title, price, rating, image */}
-      <Product
-        id="12321341"
-        title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
-        price={11.96}
-        rating={5}
-        image="https://m.media-amazon.com/images/I/61Mde-UfBOL._AC_UY218_.jpg"
-      />
+      <div className="home__row">
+        <Product
+          id="1"
+          title="Nintendo Switch Console [Neon Blue/Red]"
+          price={"448.00"}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51vKlrdRKYL._AC_UL480_SR480,480_.jpg"
+        />
+        <Product
+          id="2"
+          title="Ghost of Tsushima - PlayStation 4"
+          price={"69.00"}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/71kpwRGYwqL._AC_UL480_SR381,480_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="3"
+          title="The Last of Us Part 2 - PlayStation 4"
+          price={"68.00"}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/71T0ogpJS1L._AC_UL405_SR321,405_.jpg"
+        />
+        <Product
+          id="4"
+          title="Kindle Paperwhite – Now Waterproof with twice the Storage (8GB)"
+          price={"199.00"}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/61eAq6gg-XL._AC_UL480_SR480,480_.jpg"
+        />
+        <Product
+          id="5"
+          title="Echo Dot (3rd Gen) – Smart speaker with Alexa - Charcoal Fabric"
+          price={"69.00"}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/61MdyHSbxHL._AC_UL480_SR480,480_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="5"
+          title='LG 38WN95C 38" UWQHD+ Curved Nano IPS Monitor'
+          price={"2,299.00"}
+          rating={6}
+          image="https://images-fe.ssl-images-amazon.com/images/I/71iRe--kVtL._AC_SY400_.jpg"
+        />
+      </div>
+
       {/* Product */}
     </div>
   );
