2c5989a3c2ac0aaf719433bf27ca4123a95e7947
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-25 13:12:11 -0700

    created and styled home banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "../css/Home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>I am the home page</h1>
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/Other/RB-2759_SVOD_DiamondMultititleJuly/Amazon_GW_DesktopTallHero_RB-2759_SVOD_DiamondMultititleJuly_MovieNight_1500x600._CB407968705_.jpg"
+      />
     </div>
   );
 }
