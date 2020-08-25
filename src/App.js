caa3a5d5087206c27d8464f81879d7e9858e75d4
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-25 10:28:31 -0700

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Build an Amazon Clone</h1>
+    </div>
+  );
 }
 
 export default App;
