e69a5eb4231fa937e942d19559dfa38c1cc4ba0e
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-26 10:34:50 -0700

    setup reducer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { StateProvider } from "./components/StateProvider";
+import reducer, { initialState } from "./reducer/Reducer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider>
+    <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
   </React.StrictMode>,
