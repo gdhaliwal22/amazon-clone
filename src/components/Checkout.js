ec812dd80020a75c18a9f45e00c1c31ea4f75a13
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-26 12:24:20 -0700

    if/else message if basket is empty or not empty

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -11,6 +11,19 @@ function Checkout() {
         alt=""
         className="checkout__ad"
       />
+      {basket?.length === 0 ? (
+        <div>
+          <h2>Your Shopping Basket is empty</h2>
+          <p>
+            You have no items in your basket. To buy one or more items, click
+            "Add to basket" next to the item.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2>Your Shopping Basket</h2>
+        </div>
+      )}
     </div>
   );
 }
