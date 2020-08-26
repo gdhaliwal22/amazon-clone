0124447ba4d2c149453253fbc0269bc3d80ed878
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-26 15:23:49 -0700

    installed react-currency-format dependancy

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "../css/Subtotal.css";
 
 function Subtotal() {
-  return <div></div>;
+  return (
+    <div className="subtotal">
+      {/* Price */}
+      <button>Proceed to checkout</button>
+    </div>
+  );
 }
 
 export default Subtotal;
