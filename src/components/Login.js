4d204d25c488fb38bdfc6cc1579664728a3a087a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-27 09:41:11 -0700

    created Login.js component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,8 +4,30 @@ import { Link, useHistory } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login">
+      <Link to="/">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+          className="login__logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign In</h1>
+        <form action="">
+          <h5>E-mail</h5>
+          <input type="text"></input>
+          <h5>Password</h5>
+          <input type="password"></input>
+          <button>Sign In</button>
+        </form>
+        <p>
+          By signin-in you agree to Amazon's Conditions of Use & Sale. Please
+          see our Privacy Notice, our Cookies Notice and our Interested-Based
+          Ads Notice
+        </p>
+        <button>Create your Amazon Account</button>
+      </div>
     </div>
   );
 }
